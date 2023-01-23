da2f96a5ba88d98a6ffb55139db255d2663c05a4
MarkY1122 <mark.yousef.1@ens.etsmtl.ca>
2023-01-23 01:19:16 -0500

    Update jeuRouter-redemarrerJeu-lab0.test.ts
    partie 3 semi

--- test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,31 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
 import 'jest-extended';
+import 'jest-extended';
+import { readFileSync } from 'fs';
+const path = require('path');
+
+let content = ""
+beforeAll(async() => {
+  const filename = path.join('src', 'routes', 'redemarrerJeu.ts')
+  content = readFileSync(filename, 'utf-8');
+});
 
 describe('redemarrerJeu.test.ts', () => {
   it("should implement test", async () => {
     throw new Error("Ce test n'a pas été défini")
   });
 });
+
+describe('redemarrerJeu.test.ts', () => {
+  it("should contain \"get('/api/v1/jeu/redemarrerJeu')\"", () => {
+    expect(content.includes("get('/api/v1/jeu/redemarrerJeu')")).toBeTruthy();
+  });
+
+  it("devrait contenir un test pour jouer qui retourne 404 (après redemarrerJeu()", () => {
+    expect(content.includes(".status).toBe(200)")).toBeTruthy();
+    expect(content.includes(".response).toBe(JSON)")).toBeTruthy();
+  });
+});
+
+
